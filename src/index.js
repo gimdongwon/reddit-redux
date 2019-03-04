aa59c00b5bea318fbd0fe96fc087b8f9ce379dd1
kimdongwon <ehddnjs8989@gmail.com>
2019-03-04 19:35:17 +0900

    connect react with redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import * as serviceWorker from "./serviceWorker";
-import App from "./App";
+import App from "./components/App";
 import configureStore from "./configureStore";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import todoApp from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let store = createStore(todoApp);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import selectedReddit from "./selectedReddit";
 import postsByReddit from "./postsByReddit";
+import visibilityFilter from "./visibilityFilter";
+import todos from "./todos";
 
 const rootReducer = combineReducers({
   selectedReddit,
-  postsByReddit
+  postsByReddit,
+  todos,
+  visibilityFilter
 });
 
 export default rootReducer;
